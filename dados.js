877858a50839eae4d5691d0ffb567f6000db5960
Guilherme Filipe Nascimento <144496771+LoonneeBR@users.noreply.github.com>
2024-09-09 02:44:01 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -27,28 +27,28 @@ let dados = [
     {
         titulo:"Bambu",
         descricao: "A Vela Bambu transmite paz e tranquilidade, perfeita para meditação.",
-        tags: "paz tranquilidade meditação"
+        tags: "paz tranquilidade meditação estresse"
     },
     {
         titulo: "Canela",
         descricao: "A Vela Canela estimula a criatividade e aquece o ambiente, perfeita para momentos de inspiração.",
-        tags: "criatividade quente aconchegante"
+        tags: "criatividade quente aconchegante lar casa pensar"
     },
     {
         titulo: "Sândalo",
         descricao: "A Vela Sândalo promove relaxamento e bem-estar, ideal para momentos de introspecção.",
-        tags: "relaxamento bem-estar amadeirado"
+        tags: "relaxamento bem-estar amadeirado casa visita spa"
     },
     {
 
         titulo: "Bergamota",
         descricao: "A Vela Bergamota alivia o estresse e eleva o ânimo, perfeita para começar o dia.",
-        tags: "estresse ânimo cítrico"
+        tags: "estresse ânimo cítrico alegria energia treinar"
     },
     {
         titulo: "Flor de Laranjeira",
         descricao: "A Vela Flor de Laranjeira transmite calma e serenidade, ideal para momentos de descanso.",
-        tags: "calma serenidade floral"
+        tags: "calma serenidade floral trabalho estudar"
     }
 ];
 
