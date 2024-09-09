257a6e7aac4dc328a77ab932278df99eef48e36f
Guilherme Filipe Nascimento <144496771+LoonneeBR@users.noreply.github.com>
2024-09-09 02:28:16 -0300

    Update dados.js

--- dados.js
+++ dados.js
@@ -19,5 +19,36 @@ let dados = [
         descricao: "A Vela Lavanda promove relaxamento e tranquilidade, ideal para noites de sono.",
          tags: "dormir amor romantico"
     },
+    {
+        titulo: "Ylang Ylang",
+        descricao: "A Vela Ylang Ylang cria uma atmosfera romântica e sensual, ideal para momentos a dois.",
+        tags: "romântico sensual relaxante"
+    },
+    {
+        titulo:"Bambu",
+        descricao: "A Vela Bambu transmite paz e tranquilidade, perfeita para meditação.",
+        tags: "paz tranquilidade meditação"
+    },
+    {
+        titulo: "Canela",
+        descricao: "A Vela Canela estimula a criatividade e aquece o ambiente, perfeita para momentos de inspiração.",
+        tags: "criatividade quente aconchegante"
+    },
+    {
+        titulo: "Sândalo",
+        descricao: "A Vela Sândalo promove relaxamento e bem-estar, ideal para momentos de introspecção.",
+        tags: "relaxamento bem-estar amadeirado"
+    },
+    {
+
+        titulo: "Bergamota",
+        descricao: "A Vela Bergamota alivia o estresse e eleva o ânimo, perfeita para começar o dia.",
+        tags: "estresse ânimo cítrico"
+    },
+    {
+        titulo: "Flor de Laranjeira",
+        descricao: "A Vela Flor de Laranjeira transmite calma e serenidade, ideal para momentos de descanso.",
+        tags: "calma serenidade floral"
+    }
 ];
 
